381e2e479bd4a418b4a7b59a94c54c4a6c2fe3c4
EDA <eda@Mac-Mini-9.local>
2016-08-29 16:35:30 +1200

    login works yewotup

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ var bodyParser = require('body-parser')
 var express = require('express')
 var hbs = require('express-handlebars')
 var path = require('path')
+var expressSession = require('express-session')
+var passport = require('passport')
+var localStrategy = require('passport-local')
+var flash = require('connect-flash')
 
 var index = require('./routes/index')
 
@@ -12,8 +16,55 @@ app.engine('hbs', hbs())
 app.set('view engine', 'hbs')
 app.set('views', path.join(__dirname, 'views'))
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(passport.initialize())
 
-app.get('/', index.get)
+passport.use(new localStrategy((user, email, done) => {
+  knex('users')
+    .select()
+    .where('name', name)
+    .then(users => {
+      if (users.length === 0) {
+        return done (null, false, { message: 'Unrecongnised user.'})
+      }
+      const user = users[0]
+      if (user.email !== email) {
+        return done (null, false, { message: 'Incorrect email'})
+      }
+      done (null, user)
+      res.render('loggedIn', index.loggedIn)
+    })
+    .catch(err => done (err, false, {message: 'Database error'}))
+  }
+))
+// passport.serializeUser(localSodium.serialize)
+// passport.deserializeUser(localSodium.deserialize)
+
+// Session stuff
+app.use(expressSession({
+  resave: false,
+  secret: 'CHANGE THIS IN PRODUCTION!',
+  saveUninitialized: false
+}))
+app.use(passport.session())
+app.use(flash())
+
+app.post('/login', // Login assuming credentials are correct
+  passport.authenticate('local', {
+    successRedirect: '/', // Route index
+    failureRedirect: '/login', // Declare login page
+    failureFlash: true // No failure to be flashed
+  }))
+
+app.get('/logout', (req, res) => {
+  req.logout()
+  res.redirect('/login')
+})
+
+
+app.get('/', index.index)
+app.get('/display', index.get)
+app.get('/login', index.login)
+app.post('/login', index.postLogin)
 
 app.listen(PORT, function () {
   console.log('Listening on port', PORT)
--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,43 @@
 var express = require('express')
 var development = require('../knexfile').development
 var knex = require('knex')(development)
+var localStrategy = require('passport-local')
 
 module.exports = {
-  get: get
+  get: get,
+  index: index,
+  login: login,
+  postLogin: postLogin
+}
+
+function postLogin(req, res) {
+  console.log(req.body.name, req.body.email)
+  res.sendStatus(200)
 }
 
 function get (req, res) {
   knex('users')
     .select()
     .then(function (users) {
-      res.render('index', { users: users })
+      res.render('display', { users: users })
     })
     .catch(function (err) {
       res.status(500).send('DATABASE ERROR: ' + err.message)
     })
+}
+
+function index (req, res) {
+  res.render('index')
+}
+
+function login (req, res) {
+  res.render('login')
+}
+
+function storeSession (req, res) {
+  req.session('index')
+  }
+
+function loggedIn (req, res) {
+  req.render('loggedIn')
 }
